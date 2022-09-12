a6d12690f2b692f9279cfe3a29bc9b6e6f8f9989
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-09-12 13:04:57 +0500

    Lecture 8
    Adding templates and static files

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Class')
+    # return HttpResponse('Hello Class')
+    my_data = {'insert_here':"I am coming from view"}
+    return render(request, 'first_app/index.html', context=my_data)
--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# print(BASE_DIR)
+# BASE_DIR + '/static'
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
