b0e2bdb8ee49c4fc469035b09453806c998b4e2f
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-09-26 04:26:15 +0500

    Lecture 12
    MTV understanding

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello Class')
-    my_data = {'insert_here':"I am coming from view"}
-    return render(request, 'first_app/index.html', context=my_data)
+    # my_data = {'insert_here':"I am coming from view"}
+    # return render(request, 'first_app/index.html', context=my_data)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
