1d634c0f0d43c3ce30646b0088a969bbe48d7332
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-08 15:24:52 +0500

    Lecture 15
    Blog site forms and about us page.

--- my_blog_site/mysite/blog/views.py
+++ my_blog_site/mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post, Comment
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
--- my_blog_site/mysite/mysite/settings.py
+++ my_blog_site/mysite/mysite/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog/templates/blog')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
